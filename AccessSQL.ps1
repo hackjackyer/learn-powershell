49da209f1798ec8af56882a58098c3a88a015735
hack.jacky <hackjackyer@users.noreply.github.com>
2023-03-07 03:35:59 +0800

    Update AccessSQL.ps1

--- AccessSQL.ps1
+++ AccessSQL.ps1
@@ -16,3 +16,24 @@ Create-Database "C:\temp\test.accdb"
 
 # Create a table named PostData with two fields: ID and Content
 Invoke-ADOCommand "C:\temp\test.accdb" "CREATE TABLE PostData (ID COUNTER PRIMARY KEY, Content TEXT)"
+
+
+# Create a database connection
+$conn = New-Object -ComObject ADODB.Connection
+$conn.Open("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\temp\test.accdb")
+
+# Create a record set
+$rs = New-Object -ComObject ADODB.RecordSet
+
+# Execute a SQL query to select all records from PostData table
+$rs.Open("SELECT * FROM PostData", $conn)
+
+# Loop through the record set and display each record
+$rs | ForEach-Object {
+    Write-Host "ID: $($_.Fields.Item('ID').Value)"
+    Write-Host "Content: $($_.Fields.Item('Content').Value)"
+}
+
+# Close the record set and the connection
+$rs.Close()
+$conn.Close()
